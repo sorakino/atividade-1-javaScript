ee30ceeaa2cc5fe481e4a62af53e31e2dbceb128
Aquino <brunoaquino43@gmail.com>
2022-10-10 17:43:23 -0300

    versão 1.0

---
+++ script.js
@@ -0,0 +1,75 @@
+let nome = "Bruno";
+
+let sobrenome = "Aquino";
+
+console.log("Olá, eu me chamo " + nome + " " + sobrenome + ".");
+
+let primeiroNome = "Bruno";
+
+let segundoNome = "Aquino";
+
+let idade = 21;
+
+console.log("Olá, meu primeiro nome é " + primeiroNome + ", meu segundo nome é " + segundoNome + ", e eu tenho " + idade + " anos.");
+
+let preco1 = 20;
+
+let preco2 = 5;
+
+let precoTotal = preco1 + preco2;
+
+console.log("Comprei uma xícara por " + preco1 + " reais, e dois copos por " + preco2 + " reais. No total, gastei " + precoTotal + " reais.")
+
+let numero1 = 30;
+
+let numero2 = 7;
+
+let numeroTotal = numero1 + numero2;
+
+console.log("A soma do valor " + numero1 + " com o valor " + numero2 + " é igual a " + numeroTotal + ".")
+
+let contador = 0;
+
+console.log(contador = contador + 1);
+
+console.log(contador = contador + 1);
+
+console.log(contador = contador + 1);
+
+console.log(contador = contador + 1);
+
+console.log(contador + 1)
+
+let anoAtual = 2022;
+
+let anoNascimento = 2001;
+
+let idadeAtual = anoAtual - anoNascimento;
+
+console.log("No ano " + anoAtual + ", eu tenho/vou ter " + idadeAtual + " anos de idade ")
+
+let alturaRetangulo = 5;
+
+let larguraRetangulo = 7;
+
+let areaRetangulo = alturaRetangulo + larguraRetangulo;
+
+console.log("Um retângulo com largura de " + larguraRetangulo + "cm e altura de " + alturaRetangulo + "cm possui " + areaRetangulo + "cm² de área total.");
+
+let diaAtual = 10;
+
+let mesAtual = 10;
+
+let dataAtualFormatada = diaAtual + "/" + mesAtual + "/" + anoAtual;
+
+console.log("A data de hoje é " + dataAtualFormatada)
+
+let valorA = 10;
+
+let valorB = 2;
+
+let valorAnterior = valorA;
+
+valorA = valorB;
+
+console.log("Antes a variável A possuia o valor " +valorAnterior+ " agora possui o valor " +valorA+ " Antes a variável B, possuia o valor " +valorB+ " Agora possui o valor " +valorAnterior)
